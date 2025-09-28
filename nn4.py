5e4b50f470acfe04252da2ba46fc1f34f5006b29
fast40 <eclfast@gmail.com>
2025-09-28 14:28:16 -0700

    nn4 checkpoint

--- nn4.py
+++ nn4.py
@@ -1,13 +1,22 @@
+import uuid
+import pathlib
+
 import torch
 from torch import nn
 from torch import optim
+from torchvision.transforms import Compose, CenterCrop, ToTensor
+from PIL import Image
 
 torch.manual_seed(1337)
 
-N = 10
-D = 2
-E = 1
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+N = 100
+D = 3 * 128 * 128
+E = 2
+
 MARGIN = 1
+
 
 def differences(values):
     differences = values[:, None] - values
@@ -18,18 +27,29 @@ def distances(points):
     return (differences(points) ** 2).sum(-1)
 
 
-points = torch.rand((N, D))
+transform = Compose([CenterCrop(128), ToTensor()])
+points = torch.stack([transform(Image.open(f'celeba/{i+1:06}.jpg')).flatten() for i in range(N)])
+points = points.to(DEVICE)
+ordering = torch.where(differences(distances(points)) < 0, 1, -1)
+print(points.shape)
 
 model = nn.Embedding(N, E)
+model.to(DEVICE)
 
-optimizer = optim.SGD(model.parameters(), lr=1e-1)
+optimizer = optim.SGD(model.parameters(), lr=1e1)
+
+i = 1
 
 while True:
-    batch = points[:5]
+    embeddings = model(torch.arange(N, device=DEVICE))
 
-    embeddings = model(torch.arange(N))
+    if i % 10 == 0:
+        print('saved')
+        temp = pathlib.Path(f'model/{uuid.uuid4()}.pt')
+        with open(temp, 'wb') as file:
+            torch.save(model, file)
+        temp.replace(f'model/model_{i:06}.pt')
 
-    ordering = torch.where(differences(distances(batch)) < 0, 1, -1)
     loss = (MARGIN + differences(distances(embeddings)) * ordering).relu().mean()
 
     optimizer.zero_grad()
@@ -38,3 +58,4 @@ while True:
 
     print(loss.item())
 
+    i += 1
